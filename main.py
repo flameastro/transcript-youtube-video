9502f3d7dede456ecf5f77538ebcdf7aef328876
flameastro <gabrielgoulartbnu@gmail.com>
2026-06-26 19:38:48 -0300

    fetch: recapture new elements and change transcript.txt

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ def get_transcription(URL: str, time: bool) -> None:
     with sync_playwright() as p:
         print(f"Getting transcription... Please be patient and wait!")
 
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(URL)
 
         page.click('xpath=//*[@id="expand"]')
         page.click('xpath=//*[@id="primary-button"]/ytd-button-renderer/yt-button-shape/button')
 
-        page.wait_for_selector('xpath=//*[@id="segments-container"]/ytd-transcript-segment-renderer[1]/div/yt-formatted-string')
+        page.wait_for_selector('//*[@id="content"]/yt-section-list-renderer')
 
         i = 1
         while True:
             try:
                 if time:
-                    time = page.query_selector(f'xpath=//*[@id="segments-container"]/ytd-transcript-segment-renderer[{i}]/div/div/div').inner_text()
-                    transcription = page.query_selector(f'xpath=//*[@id="segments-container"]/ytd-transcript-segment-renderer[{i}]/div/yt-formatted-string').inner_text()
+                    time = page.query_selector(f'xpath=//*[@id="contents"]/div[{i}]/macro-markers-panel-item-view-model/timeline-item-view-model/div/transcript-segment-view-model/div[1]').inner_text()
+                    transcription = page.query_selector(f'xpath=//*[@id="contents"]/div[{i}]/macro-markers-panel-item-view-model/timeline-item-view-model/div/transcript-segment-view-model/span').inner_text()
                     with open("transcript.txt", "a", encoding="utf-8") as f:
                         f.write(f"{time}: {transcription}\n")
                 else:
-                    transcription = page.query_selector(f'xpath=//*[@id="segments-container"]/ytd-transcript-segment-renderer[{i}]/div/yt-formatted-string').inner_text()
+                    transcription = page.query_selector(f'xpath=//*[@id="contents"]/div[{i}]/macro-markers-panel-item-view-model/timeline-item-view-model/div/transcript-segment-view-model/span').inner_text()
                     with open("transcript.txt", "a", encoding="utf-8") as f:
                         f.write(f"{transcription}\n")
                 i += 1
